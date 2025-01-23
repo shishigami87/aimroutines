2a496e2a4c353d23cfa4db7a746c6b7e35bc335a
ShishigamiTV <shishigami@hotmail.de>
2025-01-23 23:59:10 +0100

    Update discord URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default async function Home() {
               size="icon"
               className="text-primary-foreground"
             >
-              <Link href="https://discord.gg/VafXRyC4zz" target="_blank">
+              <Link href="https://discord.gg/nEGEYnA6tS" target="_blank">
                 <DiscordLogoIcon />
               </Link>
             </Button>
